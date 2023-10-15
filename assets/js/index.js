3f9aeaaa89567837fdb66b84994497fc4979c405
Shane Crotty <sh.crotty@hotmail.com>
2023-10-15 17:27:50 +0000

    Add functions to index.js

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+/**
+ * Declare constants for DOM elements
+ * and possible choices
+ */
+const buttons = document.getElementsByClassName("control");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const messages = document.getElementById("messages");
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+
+
+/**
+ * Add event listener to all the buttons
+ */
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    });
+}
+
+/**
+ * The main game function. Accepts one parameter, which 
+ * is the date-choice value of the selected button
+ */
+function playGame(playerChoice) {
+
+    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice];
+
+    let computerChoice = Math.floor(Math.random() * 3);
+
+computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+computerImage.alt = choices[computerChoice];
+
+let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+updateScores(result);
+}
+
+
